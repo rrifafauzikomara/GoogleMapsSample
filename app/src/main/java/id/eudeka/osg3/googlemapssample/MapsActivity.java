8452f6ac7a11838c8995f2ed72a2f730b6dbe65e
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2020-01-20 11:45:32 +0700

    refactoring to android X

--- app/src/main/java/id/eudeka/osg3/googlemapssample/MapsActivity.java
+++ app/src/main/java/id/eudeka/osg3/googlemapssample/MapsActivity.java
@@ -1,13 +1,14 @@
 package id.eudeka.osg3.googlemapssample;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
