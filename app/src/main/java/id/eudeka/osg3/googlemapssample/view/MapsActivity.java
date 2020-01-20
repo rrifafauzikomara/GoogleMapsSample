9143b1570cd22d3f041357cb4c801f9bea1b8adb
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2020-01-20 11:40:33 +0700

    refactoring to android X

--- app/src/main/java/id/eudeka/osg3/googlemapssample/view/MapsActivity.java
+++ app/src/main/java/id/eudeka/osg3/googlemapssample/view/MapsActivity.java
@@ -9,15 +9,16 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
-import android.os.Looper;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
